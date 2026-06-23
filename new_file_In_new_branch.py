6c3cdbe163dfc3e336ec3505a0c6f52540395d9d
WilliaJAM <williamjhoanartu@gmail.com>
2026-06-23 17:07:09 -0500

    Mod new_file_In_new_branch with a new if

--- new_file_In_new_branch.py
+++ new_file_In_new_branch.py
@@ -10,8 +10,9 @@ if (num_user % 3 == 0 and num_user % 7 == 0) :
 else:
     if(num_user % 3 == 0):
         print('Fizz')
-    
-    if(num_user % 7 == 0):
+    elif(num_user % 7 == 0):
         print('Buzz')
+    else:
+        print('El Giro')
 
 print('Muy bien.., hagamos esto una ultima vez.')
