e0d004ce2b1d79bcded18d68caf3ed7b517e8815
WilliaJAM <williamjhoanartu@gmail.com>
2026-06-24 17:58:02 -0500

    New Script in the hellogit.py

--- hellogit.py
+++ hellogit.py
@@ -7,3 +7,18 @@ print(f'Hola, {input_user}')
 
 #Un XD
 
+#Terminar
+def caffeine_buzz(n):
+
+    phrase = ''
+
+    if n % 3 == 0 and n % 4 == 0 and n % 2 == 0:
+        return 'mocha_missing!'
+
+    if n % 3 == 0 and n % 4 == 0:
+        phrase = 'Coffee'
+    
+    if n % 3 == 0:
+        phrase = 'Java'
+
+    return phrase + 'Script'
