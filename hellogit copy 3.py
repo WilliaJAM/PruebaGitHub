fc3e72325cd3a4f6f82d87e6505d563935036b0b
WilliaJAM <williamjhoanartu@gmail.com>
2026-06-29 17:11:46 -0500

    An interesting commentary ?

---
+++ hellogit copy 3.py
@@ -0,0 +1,37 @@
+print('Segundo fichero')
+print('Modifiación')
+
+# input_user = input('Ingresa tu nombre')
+
+# print(f'Hola, {input_user}')
+
+#Un XD
+
+#TODO: En el ultimo if no esta valdando correctamente.
+def caffeine_buzz(n):
+
+    phrase = 'mocha_missing!'
+    validate = False
+
+    if n % 3 == 0 and n % 4 == 0:
+        phrase = 'Coffee'
+        validate = not validate
+    elif n % 3 == 0:
+        phrase = 'Java'
+        validate = not validate
+    if validate and n % 2 == 0:
+        phrase += 'Script'
+
+    return phrase
+
+print('Frase: ',caffeine_buzz(4))
+
+
+
+def new_function():
+    pass
+
+
+
+#Comentario desde el pasado
+print('Muy bien hagamos esto una ultima vez.....')
